72eb6f5e2ce66fbb915fa35f8628025e944bf068
HONEY GAUTAM <bhartichandel1517@gmail.com>
2021-01-24 20:53:28 +0530

    Final Commit to the file with all change in style sheet and script file and index.html file to complete the assignment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
+function updateSlider(sliderValue) {
+
+  var outputinterestslider = document.getElementById("outputRate");
+  outputinterestslider.innerHTML = sliderValue  + '%';
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+  
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    if(principal <=0) {
+      alert("Enter a positive number");
+    }else {
+    var interest = principal*years*rate/100;
+    var actual_year = 2021 + parseInt(years);
+     document.getElementById("result").innerHTML = 'if you deposit <mark>' + principal + '</mark><br/>' + 'at an interest rate of <mark>'+ rate + '% </mark>' +'<br/>' 
+                                                    + 'You will recieve an amount of <mark>' + interest + '<mark>,<br/>' + 'in the year <mark>' + actual_year +'</mark>';
 }
+}
+
         
